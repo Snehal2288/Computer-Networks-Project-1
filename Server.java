3fc89d6200c9bbe5eac38aa1e749efd17a943d00
Snehal2288 <snehalppatil88@gmail.com>
2016-02-12 11:45:04 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,55 +1,82 @@
+
 import java.io.*;
 import java.net.*;
-public class Client
+
+public class Server
 {
-	public static void main(String[] args)throws IOException
-	{
-		//Connect with the server
-		Socket clientsocket = new Socket("127.0.0.1",2222);
-		System.out.println("Client : Server is connected ");
-
-		int bytesinafile=0;
-		String fileinputsource = "D:\\Santa Clara University\\COEN233\\Programming Assignment\\Client_Server\\Client.txt";  
+	public static void main(String[] args) throws IOException 
+	{	   
+		ServerSocket serversocket = new ServerSocket(2222);
+		// Created server socket at port number 2222
+		System.out.println("Server is running");
+		Socket socket = serversocket.accept();
+		// Accepting client request
+		System.out.println("Client Connected");
+		String filepath = ("D:\\Santa Clara University\\COEN233\\Programming Assignment\\Client_Server\\Server.txt");
 		try
-		{
-			//read the file and store in an array
-			PrintWriter printwriter = new PrintWriter(clientsocket.getOutputStream(), true);  
-			FileInputStream fileinputstream = new FileInputStream(fileinputsource);       
-			byte[] clientarray = new byte[fileinputstream.available()];
-			System.out.println("Bytes in a file :" +fileinputstream.available());
-			bytesinafile = fileinputstream.read(clientarray);       
-			System.out.println("Number of bytes read: "+bytesinafile);
-			char[] transferarray = new char[bytesinafile];
+		{       
+			//Read the file and write on the socket
+			//Create a file if it doesn't exists
+			FileOutputStream fileoutputstream = new FileOutputStream(filepath);
+			BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			int numberofparts=bytesinafile/5;
-			if((bytesinafile%10)!= 0)
-				numberofparts++;
-			printwriter.println(numberofparts);
-			printwriter.println(bytesinafile%10);
-			printwriter.println(bytesinafile);
-			System.out.println("File will be divided into "+bytesinafile/10+ " parts");
-			int temp=0;
-
-			for(int p = 0 ; p < numberofparts ; p++)
-			{		
-				if(p==(numberofparts-1))
-					temp=bytesinafile%10;
-				else
-					temp=10;
-				int h=p*10;
-				for(int k=0;k<temp;k++)
+			String h=bufferedreader.readLine();
+			int f = Integer.parseInt(h)/2;
+			String g=bufferedreader.readLine();
+			int count=Integer.parseInt(g);
+         
+			//It gets total bytes present in a file
+			String bytesinfile=bufferedreader.readLine();
+			int temp=Integer.parseInt(bytesinfile);
+			byte[] serverarray = new byte[temp];
+			System.out.println("File will be divided into "+temp/5+ " parts");
+         
+			for(int i=0;i<f;i++)
+			{  
+				String finaloutput = bufferedreader.readLine();
+				serverarray=finaloutput.getBytes();
+         
+				System.out.println("10 bytes of data will be divided into two 5 bytes files");
+				System.out.println("First 5 bytes of data is sending ");
+				for(int q=0;q<5;q++)
 				{
-					transferarray[k]=(char)(clientarray[h+k]);
-				}  
-				String transfer=new String(transferarray);
-				printwriter.println(transfer);
-			}
-			fileinputstream.close();
+					fileoutputstream.write(serverarray[q]);
+				}
+				System.out.println("Second 5 bytes of data is sending");
+				for(int q=5;q<10;q++)
+				{ 
+					fileoutputstream.write(serverarray[q]);
+				} 
+           
+				if(i==(f))
+				{        		 
+					System.out.println("Inside f-1 loop");
+					String finaloutput2 = bufferedreader.readLine();
+					serverarray=finaloutput2.getBytes();
+					System.out.println("Sending the remaining bytes of data ");
+					if(count<5)
+					{
+						for(int q=0;q<count;q++)
+							fileoutputstream.write(serverarray[q]);
+					}
+					else
+						for(int q=5;q<count;q++)
+						{
+							for(int t=0;t<5;t++)
+								fileoutputstream.write(serverarray[t]);	
+							fileoutputstream.write(serverarray[q]);
+						}
+				}
+			}  
+         
+			System.out.println("Closing the server connection ");
+         
+      
+			fileoutputstream.close();
 		}
+	   
 		catch(Exception ex)
-		{
-		}
-		clientsocket.close();
+		{}   
+		serversocket.close();
 	}
-
 }
